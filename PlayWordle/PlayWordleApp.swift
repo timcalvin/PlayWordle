0d77f1b25b3298fba33df5b8e96861d138e14507
Tim Bryant <timcalvin@gmail.com>
2022-03-04 14:08:17 -0500

    Initial Commit

---
+++ PlayWordle/PlayWordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayWordleApp.swift
+//  PlayWordle
+//
+//  Created by Tim Bryant on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PlayWordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
